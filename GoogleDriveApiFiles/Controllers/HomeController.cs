eb375d07ce849dbf209d392e1c4c0c61cc5a883d
shawon akando <shawonaiub96@gmail.com>
2019-05-05 21:40:45 +0600

    Add project files.

---
+++ GoogleDriveApiFiles/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using GoogleDriveApiFiles.Google_Drive_Repository;
+using GoogleDriveApiFiles.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoogleDriveApiFiles.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult GetGoogleDriveFiles()
+        {
+            return View(Google_Drive_File_Repository.GetDriveFiles());
+        }
+
+        [HttpPost]
+        public ActionResult DeleteFile(GoogleDriveFiles file)
+        {
+            Google_Drive_File_Repository.DeleteFile(file);
+            return RedirectToAction("GetGoogleDriveFiles");
+        }
+
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            Google_Drive_File_Repository.FileUpload(file);
+            return RedirectToAction("GetGoogleDriveFiles");
+        }
+
+        public void DownloadFile(string id)
+        {
+            string FilePath = Google_Drive_File_Repository.DownloadGoogleFile(id);
+
+
+            Response.ContentType = "application/zip";
+            Response.AddHeader("Content-Disposition", "attachment; filename=" + Path.GetFileName(FilePath));
+            Response.WriteFile(System.Web.HttpContext.Current.Server.MapPath("~/GoogleDriveFiles/" + Path.GetFileName(FilePath)));
+            Response.End();
+            Response.Flush();
+        }
+    }
+}
---
+++ GoogleDriveApiFiles/Models/GoogleDriveFiles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoogleDriveApiFiles.Models
+{
+    public class GoogleDriveFiles
+    {
+        public string Id { set; get; }
+        public string Name { set; get; }
+        public long? Size { set; get; }
+        public long? Version { set; get; }
+        public DateTime? CreatedTime { set; get; }
+        public IList<string> Parents { set; get; }
+    }
+}
